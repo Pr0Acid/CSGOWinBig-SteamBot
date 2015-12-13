770eeafb4e35cf11c5f491587ab7a8dcf5a3bad4
TheAnthonyNL <TheAnthonyNL@users.noreply.github.com>
2015-12-13 11:55:43 +0100

    Update Bot.cs

--- SteamBot/Bot.cs
+++ SteamBot/Bot.cs
@@ -1165,7 +1165,7 @@ namespace SteamBot
                 }
                 else
                 {
-                    throw new TradeOfferEscrowDurationParseException();
+                    throw new TradeOfferEscrowDurationParseException(string.Empty);
                 }
             }
 
